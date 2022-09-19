77cfe469acbf3b5fdc3070aeae3499d8776aa3b9
Pavel Butuzov <pavel.butuzov@testit.software>
2022-09-19 15:50:06 +0300

    Added changes from code review.

--- testit-adapter-cucumber/src/app-properties.ts
+++ testit-adapter-cucumber/src/app-properties.ts
@@ -22,8 +22,8 @@ export class AppPropetries {
             console.warn('The configuration file specifies a private token. It is not safe. Use TMS_PRIVATE_TOKEN environment variable');
         }
 
-        properties = this.mergeProperties(properties, this.loadCliProperties());
         properties = this.mergeProperties(properties, this.loadEnvProperties());
+        properties = this.mergeProperties(properties, this.loadCliProperties());
 
         return this.validateProperties(properties);
     }
