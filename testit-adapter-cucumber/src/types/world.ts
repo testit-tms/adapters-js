c2923daf22ae7db93fe86f0c3cde8b3748dfa695
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2023-08-31 17:38:49 +0300

    Remove unused

--- testit-adapter-cucumber/src/types/world.ts
+++
@@ -1,8 +0,0 @@
-import { World } from '@cucumber/cucumber';
-import { LinkPost } from 'testit-api-client';
-
-export interface TestItWorld extends World {
-  addMessage(message: string): void;
-  addLinks(links: LinkPost[]): void;
-  addAttachments(attachments: string[]): void;
-}
