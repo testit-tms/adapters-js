e73b2748494b60ea038bba3f5179dc2158787950
Pavel Butuzov <73579433+PavelButuzov@users.noreply.github.com>
2024-11-19 17:13:25 +0300

    TMS-34: Removed the use of the api-client. Added the use of the strategy from the commons package. (#78)
    Co-authored-by: pavel.butuzov <pavel.butuzov@testit.software>

--- testit-adapter-codecept/src/bootstrap.ts
+++ testit-adapter-codecept/src/bootstrap.ts
@@ -1,32 +1,27 @@
 import { container, event, recorder } from "codeceptjs";
-import { Client, ConfigComposer } from "testit-js-commons";
-import { Logger } from "./common/classes";
-import { StrategyFactory } from "./strategies";
+import { ConfigComposer, StrategyFactory } from "testit-js-commons";
+import { ResultBuilder, TestsBuilder } from "./builders";
+import { isPassed } from "./common/utils";
 
 module.exports = async function (options) {
   const config = new ConfigComposer().compose();
-  const client = new Client(config);
-  const logger = new Logger(options.__DEV ?? false);
 
-  const strategy = StrategyFactory.create(client, logger, config);
+  const strategy = StrategyFactory.create(config);
   const helper = container.helpers("TestITHelper");
 
-  await strategy.bootstrap();
-
-  event.dispatcher.on(event.test.after, (test) => {
-    strategy.collect(test.id, helper.metadata);
-    helper.metadata = {};
-  });
-
-  event.dispatcher.on(event.test.before, async (test) => {
-    await strategy.beforeTest(test);
-  });
+  await strategy.setup();
 
   event.dispatcher.on(event.test.finished, (test) => {
     recorder.add("transferTestAndRuns", async () => {
-      const suite = { tests: [test] };
-      await strategy.transferTestsToSystem(suite);
-      await strategy.transferRunsToSystem(suite);
+      const autotest = TestsBuilder.build(test);
+
+      await strategy.loadAutotest(autotest, isPassed(test));
+
+      const result = await new ResultBuilder(config).build(test, helper.metadata);
+
+      await strategy.loadTestRun([result]);
+
+      helper.metadata = {};
     });
   });
 
--- testit-adapter-codecept/src/strategies/index.ts
+++
@@ -1,5 +0,0 @@
-export { BaseStrategy } from "./base.strategy";
-export { DefaultStrategy } from "./default.strategy";
-export { PartialStrategy } from "./partial.strategy";
-export { ScratchStrategy } from "./scratch.strategy";
-export { AdapterMode, StrategyFactory } from "./strategy.factory";
