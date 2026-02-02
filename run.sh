1ae25115585779f5022eb21d2c4dee9c4883c29d
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-02-02 17:03:10 +0300

    feat: tms 5.6 support

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=3.6.0-TMS-5.6
+API_CLIENT_VERSION=7.1.0-TMS-5.6
+
+node update_version.js $VERSION
+
+# Update testit-api-client version in package.json using sed
+sed -i "s/\"testit-api-client\": \".*\"/\"testit-api-client\": \"$API_CLIENT_VERSION\"/g" testit-js-commons/package.json
